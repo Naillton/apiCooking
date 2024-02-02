c76877187f27433feccd9a5edc9cfd66e2ec47de
Naillton <nailton_junior@protonmail.com>
2024-02-02 16:41:38 -0300

    first commit

---
+++ build.gradle.kts
@@ -0,0 +1,34 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.2.2"
+	id("io.spring.dependency-management") version "1.1.4"
+}
+
+group = "com.nailton"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	sourceCompatibility = JavaVersion.VERSION_17
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-jdbc")
+	implementation("org.springframework.boot:spring-boot-starter-jersey")
+	implementation("org.springframework.boot:spring-boot-starter-security")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("com.auth0:java-jwt:4.4.0")
+	implementation("jakarta.persistence:jakarta.persistence-api:3.2.0-M1")
+	runtimeOnly("com.mysql:mysql-connector-j")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.springframework.security:spring-security-test")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
---
+++ src/main/java/com/nailton/apiCooking/ApiCookingApplication.java
@@ -0,0 +1,13 @@
+package com.nailton.apiCooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCookingApplication.class, args);
+	}
+
+}
